b7f045729c11c08434ee1f838b0abde16e112483
david98m <damc91@gmail.com>
2026-04-23 22:35:37 -0500

    Se crea funcion limpieza_perfiles

--- limpieza.py
+++ limpieza.py
@@ -28,3 +28,42 @@ def limpiar_usuarios(df_usuarios):
 
     # index=False evita guardar el índice de Pandas en el archivo
     df_usuarios.to_csv('data/proccesed/usuarios_limpios.csv', index=False)
+
+def limpieza_perfiles(df_perfiles):
+    print('Limpieza de espacio y texto en formato titulo')
+    df_perfiles['direccion'] = df_perfiles['direccion'].str.strip()
+
+    print('Reemplazar datos con un valor')
+    #Reemplazar datos con un valor
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_1', 1)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_2', 2)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_3', 3)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_17', 17)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_29', 29)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_32', 32)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_35', 35)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_38', 38)
+    df_perfiles['id'] = df_perfiles['id'].replace('ID_49', 49)
+
+    print('Reemplazar datos con un valor')
+    #Reemplazar datos con un valor
+    # Convertir 'N/A' y celdas vacías en nulos
+    df_perfiles['telefono'] = df_perfiles['telefono'].replace(['N/A'], np.nan)
+    df_perfiles['telefono'] = df_perfiles['telefono'].replace([''], np.nan)
+    df_perfiles['telefono'] = df_perfiles['telefono'].replace([' '], np.nan)
+    df_perfiles['direccion'] = df_perfiles['direccion'].replace("", "Sin dirección registrada")
+
+    # Explicación del r'[^\d]': Busca cualquier cosa que NO sea un número y la borra
+    df_perfiles['telefono'] = df_perfiles['telefono'].astype(str).str.replace(r'[^\d]', '', regex=True)
+
+    # El paso anterior convierte los NaN en la palabra "nan", hay que devolverlos a nulos
+    df_perfiles['telefono'] = df_perfiles['telefono'].replace('nan', np.nan)
+
+    # . Rellenar los que quedaron vacíos después de la limpieza
+    df_perfiles['telefono'] = df_perfiles['telefono'].fillna(00000000)
+
+    # Eliminar el 57 al inicio del número de teléfono
+    df_perfiles['telefono'] = df_perfiles['telefono'].str.lstrip('57')
+
+    # index=False evita guardar el índice de Pandas en el archivo
+    df_perfiles.to_csv('data/proccesed/perfiles_limpios.csv', index=False)
